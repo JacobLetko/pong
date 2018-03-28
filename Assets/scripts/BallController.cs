919e705aa289974417601dfc3416aec201546235
Jacob Letko <s179061@students.aie.edu.au>
2018-03-27 17:42:35 -0700

    not finished

---
+++ Assets/scripts/BallController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+
+    Rigidbody rb;
+    public float speed;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Invoke("startBall", 0);
+    }
+
+    void startBall()
+    {
+        rb.AddForce(new Vector3(speed * -1, 0, speed), ForceMode.Impulse);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == ("Paddle"))
+            rb.velocity = new Vector3(collision.relativeVelocity.x, collision.relativeVelocity.y, collision.relativeVelocity.z);
+        else
+        {
+            Vector3 hold = rb.velocity;
+            //hold.z = (rb.velocity.z / 2f) + (collision.collider.attachedRigidbody.velocity.z / 3f);
+            hold.z *= -1;
+            rb.AddForce(Vector3.forward * Mathf.Sign(hold.z) * 1, ForceMode.Impulse);
+            speed = speed + .01f;
+            rb.velocity = hold * speed;
+        }
+    }
+}
---
+++ Assets/scripts/playerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class playerController : MonoBehaviour {
+
+    public float speed;
+    Rigidbody rb;
+    Scene name;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        name = SceneManager.GetActiveScene();
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (name.name == "2d pong")
+        {
+            if (Input.GetKey(KeyCode.A)) //Left
+            {
+                rb.velocity = new Vector3(0, 0, speed);
+            }
+
+            else if (Input.GetKey(KeyCode.D)) //Right
+            {
+                rb.velocity = new Vector3(0, 0, -speed);
+            }
+            else
+                rb.velocity = new Vector3(0, 0, 0);
+        }
+    }
+}
+/*
+
+    for()
+    if i == score
+    [].game.set(true)
+
+    [i].game.set(false)
+
+ * /
